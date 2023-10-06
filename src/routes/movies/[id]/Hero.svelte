07a2713e5bb9df6bd1d05379f82c986d359594b0
Jean-Michel <jmgambard@wamiz.com>
2023-10-06 09:57:19 +0200

    wip movie page

--- src/routes/movies/[id]/Hero.svelte
+++ src/routes/movies/[id]/Hero.svelte
@@ -1,5 +1,5 @@
 <script lang="ts">
-	import { IMAGE_BASE_URL, media } from "$lib/api";
+	import { media } from "$lib/api";
 	import type { MovieDetails } from "$lib/types";
 
     export let movie: MovieDetails;
@@ -15,3 +15,54 @@
         <p>{movie.overview}</p>
     </div>
 </div>
+
+<style>
+    .hero {
+        display: grid;
+        background: black;
+    }
+
+    .backdrop img {
+            width: 100%;
+    }
+
+
+    .backdrop {
+        position: relative;
+    }
+
+    .backdrop::after {
+        content: '';
+        position: absolute;
+        width: 100%;
+        height: 5rem;
+        bottom: 0;
+        left: 0;
+        background: linear-gradient(to top, black, transparent);
+    }
+
+    .info {
+            display: flex;
+            flex-direction: column;
+            padding: 0 1rem;
+            gap: 1rem;
+            margin-top: -6rem;
+            z-index: 1;
+    }
+
+    @media (min-width: 50rem) {
+        .hero {
+            grid-template-columns: 1fr 50rem;
+            grid-template-rows: auto;
+        }
+
+        .info * {
+            margin: 0;
+        }
+
+        .backdrop {
+            position: relative;
+            grid-column: 2/3;
+        }
+    }
+</style>
